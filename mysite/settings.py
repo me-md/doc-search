fb60ff93ad48aa6cd738f897b7dcfc5a76c71743
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-12-19 14:44:32 -0700

    Add localhost to allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ec2-3-136-45-48.us-east-2.compute.amazonaws.com']
+ALLOWED_HOSTS = ['ec2-3-136-45-48.us-east-2.compute.amazonaws.com', 'localhost']
 
 
 # Application definition
