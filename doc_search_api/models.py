4ec404e62a2f2867497723ec73c7b5e0768f0353
Tylor Schafer <tylorschafer@gmail.com>
2019-12-18 17:02:42 -0700

    Add LocationCondition model

--- doc_search_api/models.py
+++ doc_search_api/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class LocationCondition(models.Model):
+    location = models.Charfield(max_length=500)
+    condition = models.Charfield(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    modified_at = models.DateTimeField(auto_now=True, editable=False)
