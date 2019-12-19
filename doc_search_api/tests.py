a6f82c1a533243f5dc6a8b2cdcbad3a98ddb5d80
Tylor Schafer <tylorschafer@gmail.com>
2019-12-18 19:10:36 -0700

    Add LocationCondition model test

--- doc_search_api/tests.py
+++ doc_search_api/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+import pytest
+import expects
 
-# Create your tests here.
+from .models import LocationCondition
+
+class LocationConditionModelTest(TestCase):
+    def test_creation(self):
+        ls = LocationCondition(location="Colorado", condition="Obesity")
+        self.assertEqual(ls.location, "Colorado")
+        self.assertEqual(ls.condition, "Obesity")
