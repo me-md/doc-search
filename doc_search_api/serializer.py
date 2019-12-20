72a821eba029f9d8193b4c3d182c024c4e4394da
Tylor Schafer <tylorschafer@gmail.com>
2019-12-19 19:56:13 -0700

    Add full doctor serializer

--- doc_search_api/serializer.py
+++ doc_search_api/serializer.py
@@ -1,3 +1,10 @@
 from rest_framework import serializers
+import json
+from doc_search_api.objects import Doctor
 
-class DocSerializer(serializers.Serializer):
+class DocSerializer():
+    def doctors(doc_data):
+        docs = []
+        for doc in doc_data['data']:
+            docs.append(Doctor(doc))
+        return json.dumps(docs)
