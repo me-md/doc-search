8bc600aa2907a82cf87251a9d8e4f1ff9304f8ef
Tylor Schafer <tylorschafer@gmail.com>
2019-12-19 22:45:51 -0700

    Refactor doc_service test to instansiate a class first

--- doc_search_api/tests/better_doctor_service_test.py
+++ doc_search_api/tests/better_doctor_service_test.py
@@ -4,7 +4,7 @@ from expects import *
 from doc_search_api.services.better_doctor_service import DocService
 
 def test_it_works():
-    result = json.loads(DocService.all_docs('co-denver'))
+    result = json.loads(DocService().all_docs('co-denver'))
 
     expect(result['meta']['data_type']).to(equal('array'))
     expect(result['data'][0]['practices'][0]['uid']).to(equal('ca3466def4a60da36654c49bf01ef94b'))
