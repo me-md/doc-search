90ed4eedc76d6cc623e668cd303c6735efbca2e1
Tylor Schafer <tylorschafer@gmail.com>
2019-12-19 19:35:22 -0700

    Add doctor object and serializer

---
+++ doc_search_api/objects/doctor.py
@@ -0,0 +1,21 @@
+class Doctor:
+    def __init__(self, data):
+        self.practice = load_practice(data)
+        self.profile = load_profile(data)
+
+    def load_practice(data):
+        result = {}
+        result['name']
+        result['location']
+        result['lat']
+        result['lon']
+        result['city']
+        result['state']
+        result['street']
+        result['street2']
+        result['zip']
+        result['uid']
+        result['accepts_new_patients']
+        result['insurance_uids']
+
+    
