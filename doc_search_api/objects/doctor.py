7195ec31894469258bb10ed9cf878233068e6d97
Tylor Schafer <tylorschafer@gmail.com>
2019-12-19 22:43:08 -0700

    Add doctor_data view

--- doc_search_api/objects/doctor.py
+++ doc_search_api/objects/doctor.py
@@ -1,9 +1,16 @@
-class Doctor:
-    def __init__(self, data):
-        self.practice = load_practice(data)
-        self.profile = load_profile(data)
+from django.db import models
 
-    def load_practice(data):
+class Doctor(models.Model):
+
+    def __init__(self, data):
+        self.name = 'Doctor Data'
+        self.practice = self.load_practice(data)
+        self.profile = self.load_profile(data)
+
+    def __str__(self):
+        return self.name
+
+    def load_practice(self, data):
         result = {}
         result['name'] = data['practices'][0]['name']
         result['location'] = data['practices'][0]['location_slug']
@@ -12,21 +19,24 @@ class Doctor:
         result['city'] = data['practices'][0]['visit_address']['city']
         result['state'] = data['practices'][0]['visit_address']['state']
         result['street'] = data['practices'][0]['visit_address']['street']
-        result['street2'] = data['practices'][0]['visit_address']['street2']
+        # result['street2'] = data['practices'][0]['visit_address']['street2']
         result['zip'] = data['practices'][0]['visit_address']['zip']
         result['uid'] = data['practices'][0]['uid']
         result['accepts_new_patients'] = data['practices'][0]['accepts_new_patients']
         result['insurance_uids'] = data['practices'][0]['insurance_uids']
         return result
 
-    def load_profile(data):
+    def load_profile(self, data):
         result = {}
         result['first_name'] = data['profile']['first_name']
-        result['middle_name'] = data['profile']['middle_name']
+        # result['middle_name'] = data['profile']['middle_name']
         result['last_name'] = data['profile']['last_name']
         result['title'] = data['profile']['title']
-        result['school'] = data['educations'][0]['school']
+        result['school'] = data['educations']
         result['image_url'] = data['profile']['image_url']
-        result['gender'] = data['profile']['gender']
+        # result['gender'] = data['profile']['gender']
         result['bio'] = data['profile']['bio']
         return result
+
+    class Meta:
+        abstract = True
--- doc_search_api/serializer.py
+++ doc_search_api/serializer.py
@@ -1,10 +1,13 @@
 from rest_framework import serializers
+from django.http import HttpResponse
 import json
-from doc_search_api.objects import Doctor
+from doc_search_api.objects.doctor import Doctor
+from doc_search_api.formatters.doctors_formatter import DoctorFormatter
 
 class DocSerializer():
     def doctors(doc_data):
         docs = []
-        for doc in doc_data['data']:
+        format = json.loads(doc_data)
+        for doc in format['data']:
             docs.append(Doctor(doc))
-        return json.dumps(docs)
+        return HttpResponse(DoctorFormatter().format(docs))
