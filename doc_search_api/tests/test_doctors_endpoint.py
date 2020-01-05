436f2168974159db9b0b826df824bdc139ab90ae
Tylor Schafer <tylorschafer@gmail.com>
2020-01-04 19:02:35 -0700

    Add additional doctors testing

--- doc_search_api/tests/test_doctors_endpoint.py
+++ doc_search_api/tests/test_doctors_endpoint.py
@@ -21,6 +21,10 @@ class GetAllDoctorsTest(BaseViewTest):
         expect(data[0]['practice']['lat']).to(equal(39.7392))
         expect(data[0]['practice']['lon']).to(equal(-104.94187))
 
+        expect(data[0]['profile']['first_name']).to(equal('Esther'))
+        expect(data[0]['profile']['middle_name']).to(equal('L'))
+        expect(data[0]['profile']['last_name']).to(equal('Langmack'))
+
     def test_doctors_endpoint_requires_location(self):
         response = self.client.get(
             reverse('doctors-all')
