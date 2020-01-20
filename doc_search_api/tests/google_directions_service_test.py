801b5842db27d6d15069a163f8031e13549690f4
Tylor Schafer <tylorschafer@gmail.com>
2020-01-20 13:33:52 -0700

    Add GoogleDistanceService testing

---
+++ doc_search_api/tests/google_directions_service_test.py
@@ -0,0 +1,13 @@
+import json
+import pytest
+from expects import *
+from doc_search_api.services.better_doctor_service import DocService
+
+def test_it_works():
+    result = json.loads(GoogleDistanceService().distances(40.714224,-73.961452, 39.720454,-104.898462,39.73094,-104.93483))
+
+    expect(result['destination_addresses'][0].to(equal('130 Rampart Way, Denver, CO 80230, USA'))
+    expect(result['destination_addresses'][1].to(equal('880 Clermont St, Denver, CO 80220, USA'))
+    expect(result['origin_addresses'][0].to(equal('279 Bedford Ave, Brooklyn, NY 11211, USA'))
+    expect(result['rows'][0]['elements'][0]['distance']['text'].to(equal('1,784 mi'))
+    expect(result['rows'][0]['elements'][1]['distance']['text'].to(equal('1,780 mi'))
