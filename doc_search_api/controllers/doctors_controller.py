52eeb185edf53406e2d60311f641456db1a6e2ae
Tylor Schafer <tylorschafer@gmail.com>
2020-01-21 22:08:01 -0700

    Add templorary time stamps to track perforamnce

--- doc_search_api/controllers/doctors_controller.py
+++ doc_search_api/controllers/doctors_controller.py
@@ -1,8 +1,13 @@
 from doc_search_api.views import doctors
 from doc_search_api.facades.doctors_facade import DoctorsFacade
 from django.http import JsonResponse
+from datetime import datetime
 
 def index(request):
+    start = datetime.now()
+    start_time = start.strftime("%S")
+    print("Start", start_time)
+
     location = request.GET['location'] if 'location' in request.GET else None
     provider = request.GET['provider'] if 'provider' in request.GET else None
     lat = request.GET['lat'] if 'lat' in request.GET else None
@@ -13,7 +18,13 @@ def index(request):
         return doctors(data, provider, coords, location)
     elif location:
         data = DoctorsFacade().doctors(location)
-        return doctors(data, None, coords, location)
+        response = doctors(data, None, coords, location)
+        # return doctors(data, None, coords, location)
+        end = datetime.now()
+        end_time = end.strftime("%S")
+        print("Stop!", end_time)
+
+        return response
     else:
         return JsonResponse(
             {'error': 'Must Supply a location in query params.'}
