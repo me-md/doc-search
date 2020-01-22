f041b7223905c9400bfee3d727885674934e3b6c
Tylor Schafer <tylorschafer@gmail.com>
2020-01-21 22:24:25 -0700

    Remove monitoring code

--- doc_search_api/controllers/doctors_controller.py
+++ doc_search_api/controllers/doctors_controller.py
@@ -4,10 +4,6 @@ from django.http import JsonResponse
 from datetime import datetime
 
 def index(request):
-    start = datetime.now()
-    start_time = start.strftime("%S.%f")
-    print("Start", start_time)
-
     location = request.GET['location'] if 'location' in request.GET else None
     provider = request.GET['provider'] if 'provider' in request.GET else None
     lat = request.GET['lat'] if 'lat' in request.GET else None
@@ -19,12 +15,7 @@ def index(request):
     elif location:
         data = DoctorsFacade().doctors(location)
         response = doctors(data, None, coords, location)
-        # return doctors(data, None, coords, location)
-        end = datetime.now()
-        end_time = end.strftime("%S.%f")
-        print("Stop!", end_time)
-
-        return response
+        return doctors(data, None, coords, location)
     else:
         return JsonResponse(
             {'error': 'Must Supply a location in query params.'}
